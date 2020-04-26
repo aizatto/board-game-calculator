14b2ad79f902a71aa0c483f6c68a03448dc7077b
Aizat Faiz <aizat.faiz@gmail.com>
2020-04-26 19:32:52 +0800

    Updates

---
+++ precache-manifest.9c51550d7199303213a1d9019bbca2d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7eaf925b0d174c5f6c791aa90fb9b5",
+    "url": "/board-game-calculator/index.html"
+  },
+  {
+    "revision": "ef6839356304ac9edf0c",
+    "url": "/board-game-calculator/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "5f6bfdc3dcad7350aea0",
+    "url": "/board-game-calculator/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ef6839356304ac9edf0c",
+    "url": "/board-game-calculator/static/js/2.3ecd68d0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/board-game-calculator/static/js/2.3ecd68d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6bfdc3dcad7350aea0",
+    "url": "/board-game-calculator/static/js/main.9df0ba4e.chunk.js"
+  },
+  {
+    "revision": "d36fbb7168857c21983b",
+    "url": "/board-game-calculator/static/js/runtime-main.0d21590e.js"
+  }
+]);
